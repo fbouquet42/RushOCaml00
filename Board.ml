0b93c4f5f55ff313849f2dd8a90b34200b79cacf
Felix BOUQUET <fbouquet@e1r2p22.42.fr>
2018-10-06 17:04:26 +0200

    display Board

--- Board.ml
+++ Board.ml
@@ -10,23 +10,37 @@ let create () =
   loop 9
 
 let display board =
-  let rec loop lst n =
-    match lst with
-    | [] -> ()
-    | x :: xs when n = 0 -> begin
-      (*print_string "TEST "; print_int n;*)
-      Grid.display x n;
-      print_string "\n";
-      loop xs 2
-    end
-    | x :: xs -> begin
-      (*print_string "TEST "; print_int n;*)
-      Grid.display x n;
-      print_string " | ";
-      loop xs (n - 1)
-    end
+  let rec lines head =
+      let rec loop board y x =
+          match board with
+          | [elem] when y = 2 && x = 2 ->
+                  begin
+                      Grid.display elem x;
+                      print_char '\n'
+                  end
+          | elem :: tail when y = 2 && x = 2 ->
+                  begin
+                      Grid.display elem x;
+                      print_endline "\n---------------------";
+                      lines tail
+                  end
+          | elem :: tail when x = 2 ->
+                  begin
+                      Grid.display elem x;
+                      print_char '\n';
+                      loop head (y + 1) 0
+                  end
+          | elem :: tail ->
+                  begin
+                      Grid.display elem x;
+                      print_string " | ";
+                      loop tail y (x + 1)
+                  end
+          | _ -> ()
+      in
+      loop board 0 0
   in
-  loop board 2
+  lines board
 
 let resolve board player coord = false
 
